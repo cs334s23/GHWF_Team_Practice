6d12a9a6bb0c9be651c0c38281aad61572924857
Justin Szaro <justinszaro@gmail.com>
2023-01-23 14:48:21 -0500

    Created tests for doubling numbers.

---
+++ tests/test_double_number.py
@@ -0,0 +1,8 @@
+import pytest
+#import ../double_number.py
+
+def test_double_number():
+    #assert double_number.double_number(3) == 6
+    #assert double_number.double_number(10) == 20
+    #assert double_number.double_number(0) == 0
+    return 0
